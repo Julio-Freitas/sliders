4ebebeed73e27a237f84a4914ded6e8d1d1cc134
Julio-Freitas <julio.clf31@gmail.com>
2022-01-31 14:09:21 -0300

    feat(project): mount project slider

---
+++ debounce/debounce.js
@@ -0,0 +1,10 @@
+export default function debounce(calback, delay) {
+  let timer;
+
+  return (...args) => {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      calback(args);
+    }, delay);
+  };
+}
